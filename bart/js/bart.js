90ca4e9d413c38bcc906adce6b39fe010164a476
Chris Gross <christopher.gross@gmail.com>
2014-12-06 14:48:05 -0800

    Clean up and different alert colors

--- bart/js/bart.js
+++ bart/js/bart.js
@@ -3,37 +3,21 @@
 
 $(function () {
     'use strict';
-    
-    //$('p').each(function () {
-    //    $(this).html('<b>' + this.innerHTML + '</b>');
-    //});
-    //$HTML.before('<b>TEST</b>');
-    
-    //$('p:first').html('<b>' + $HTML + '</b>');
-    
-    //$('p:last').html().after('</b>');
     var BART = 'http://api.bart.gov/api/bsa.aspx', Key = 'MW9S-E7SL-26DU-VV8V';
-    
-    //$.get(BART, 'cmd=bsa&key=' + Key + '&date=today', function (data) {
-        //e.preventDefault();
-        //$('p:last').after('<p>' + data + '</p>');
-        //$('p:last').html(data);
-        //var xmlDoc = $.parseXML(data), $xml = $(xmlDoc), $desc = $xml.find('description');
         
     $.get(BART, {cmd: 'bsa', key: Key, date: 'today'}, function (data) {
         var type = $(data).find('type').text();
         
         if (type === 'DELAY') {
             // Display yellow delay alert
-            $('p:last').after('<p>' + $(data).find('description').text() + '</p>');
+            $('p:last').after('<div class="alert alert-warning" role="alert">' + $(data).find('description').text() + '</div>');
         } else if (type === 'EMERGENCY') {
             // Display red delay alert
-            $('p:last').after('<p>' + $(data).find('description').text() + '</p>');
+            $('p:last').after('<div class="alert alert-danger" role="alert">' + $(data).find('description').text() + '</div>');
         } else {
             // Display green alert box - everything should be OK if we get here
             $('p:last').after('<div class="alert alert-success" role="alert">' + $(data).find('description').text() + '</div>');
         }
-        //$('p:last').after('<p>' + $(data).find('time').text() + '</p>');
     });
     
 });
